a6fe524d42cafd839d99db22cbdbe61f20a143b3
vincent-vianto <vincentvianto@gmail.com>
2020-07-10 14:56:20 +0700

    80%

---
+++ js/index.js
@@ -0,0 +1,25 @@
+if (typeof document.onselectstart != "undefined") {
+    document.onselectstart = new Function("return false");
+} else {
+    document.onmousedown = new Function("return false");
+    document.onmouseup = new Function("return true");
+}
+
+document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+new WOW().init();
