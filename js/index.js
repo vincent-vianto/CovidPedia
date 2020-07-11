7b32d00b7bcf6a4f44fdbb2b09d45822e1d47be2
vincent-vianto <vincentvianto@gmail.com>
2020-07-11 15:11:49 +0700

    set up button read more

--- js/index.js
+++ js/index.js
@@ -6,23 +6,23 @@ if (typeof document.onselectstart != "undefined") {
     document.onmouseup = new Function("return true");
 }
 
-document.onkeydown = function (e) {
-    if (event.keyCode == 123) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
-}
+// document.onkeydown = function (e) {
+//     if (event.keyCode == 123) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//         return false;
+//     }
+// }
 
 let myVar;
 
@@ -34,5 +34,16 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("unloader").style.display = "block";
 }
+
+$(document).ready(function() {
+    $('[data-toggle="collapse"]').click(function() {
+      $(this).toggleClass( "active" );
+      if ($(this).hasClass("active")) {
+        $(this).text("Hide");
+      } else {
+        $(this).text("Read More");
+      }
+    }); 
+    });
 
 new WOW().init();
